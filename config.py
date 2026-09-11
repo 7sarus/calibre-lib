1c1d6a356ebb4e32b95d45ddd6ba387a854b87e1
7sarus <63068332+7sarus@users.noreply.github.com>
2026-09-12 02:40:56 +0530

    release: v1.0.1 'Godzila' hotfix

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ from qt.core import (
 )
 
 # Plugin Version definitions
-PLUGIN_VERSION = (1, 0, 2, "b")
-PLUGIN_VERSION_STR = "v1.0.2b"
+PLUGIN_VERSION = (1, 0, 1, "release")
+PLUGIN_VERSION_STR = "v1.0.1"
 
 # Store configuration under Calibre's standard plugin config path
 prefs = JSONConfig("plugins/libgen_store")
